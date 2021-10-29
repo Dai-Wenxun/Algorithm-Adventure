e0d6e9e0866104bcc846a56d4523e794b57e0080
Dai-Wenxun <wxDai2001@gmail.com>
2021-10-29 21:08:17 +0800

    Update 234. 回文链表.cpp

--- 递归/234. 回文链表.cpp
+++ 递归/234. 回文链表.cpp
@@ -17,3 +17,47 @@ public:
 		return res;
 	} 
 };
+
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+    	if (head->next == NULL)
+    		return true;
+    	
+		ListNode *p1, *p2;
+		p1 = p2 = head;
+					
+		while(p2 != NULL && p2->next != NULL) {
+			p1 = p1->next;
+			p2 = p2->next->next;
+		}
+		
+		if (p2 != NULL)
+			p1 = p1->next;
+		
+		
+		ListNode *last = reverse(p1);
+		
+		while(last != NULL) {
+			if (head->val != last->val)
+				return false;
+			head = head->next;
+			last = last->next;
+		}
+		
+		return true;	
+	}
+	
+	ListNode *reverse(ListNode *head) {
+		ListNode *pre = NULL, *cur = head;
+		
+		while(cur != NULL) {
+			ListNode *nxt = cur->next;
+			cur->next = pre;
+			pre = cur;
+			cur = nxt;
+		}
+		
+		return pre;
+	}
+};
