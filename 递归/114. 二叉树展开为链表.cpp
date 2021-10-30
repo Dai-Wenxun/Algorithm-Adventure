5478481945c0a0c8e35f721255185d9c68726af2
Dai-Wenxun <wxDai2001@gmail.com>
2021-10-30 20:31:58 +0800

    Create 114. 二叉树展开为链表.cpp

---
+++ 递归/114. 二叉树展开为链表.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+		if (root ==  NULL)
+			return ;
+		
+		flatten(root->left);
+		flatten(root->right);
+		
+		TreeNode *left = root->left;
+		TreeNode *right = root->right;
+			
+		root->left = NULL;
+		root->right = left;
+		
+		TreeNode *p = root;
+	
+		while(p->right != NULL) {
+			p = p->right;
+		}
+	
+		p->right = right;
+	}
+};
