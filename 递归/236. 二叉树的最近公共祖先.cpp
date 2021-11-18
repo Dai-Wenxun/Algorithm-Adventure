2ed91149882d3206cbdba7621080bd82abaa6181
Dai-Wenxun <wxDai2001@gmail.com>
2021-11-18 16:18:18 +0800

    Create 236. 二叉树的最近公共祖先.cpp

---
+++ 递归/236. 二叉树的最近公共祖先.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    	if (root == NULL)
+    		return NULL;
+    		
+    	if (root == p || root == q)
+    		return root;
+    	
+        auto left =  lowestCommonAncestor(root->left, p, q);
+        auto right = lowestCommonAncestor(root->right, p, q);
+    
+		if (left != NULL && right != NULL)
+			return root;
+					
+		if (left == NULL && right == NULL)
+			return NULL;
+			
+		return left == NULL ? right: left;
+	}
+};
