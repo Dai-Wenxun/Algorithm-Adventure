501269432ec1c1ef5db4071ff81e9dd65fbcaa76
Dai-Wenxun <wxDai2001@gmail.com>
2021-11-20 19:54:16 +0800

    Create 107. 二叉树的层序遍历 II.cpp

---
+++ 树/中等/107. 二叉树的层序遍历 II.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		vector<vector<int>> v;
+		if (root == NULL)
+			return v;
+		
+		queue<TreeNode *> q;
+		q.push(root);
+		TreeNode *cur_node;
+		while(!q.empty()) {
+			int size = q.size();
+			vector<int> nums;
+			for (int i = 0; i < size; ++i) {
+				cur_node = q.front();
+				q.pop();
+				if (cur_node->left != NULL)
+					q.push(cur_node->left);
+				if (cur_node->right != NULL)
+					q.push(cur_node->right);
+				
+				nums.push_back(cur_node->val);
+			}
+			v.push_back(nums);
+		}
+		
+		reverse(v.begin(), v.end());
+		return v;
+	}
+};
