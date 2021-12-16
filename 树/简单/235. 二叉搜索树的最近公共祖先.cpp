8bb681df7d8b8f9a5e21b25718ae01e63bad2355
Dai-Wenxun <wxDai2001@gmail.com>
2021-12-16 20:15:43 +0800

    Update 235. 二叉搜索树的最近公共祖先.cpp

--- 树/简单/235. 二叉搜索树的最近公共祖先.cpp
+++ 树/简单/235. 二叉搜索树的最近公共祖先.cpp
@@ -3,10 +3,8 @@ public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if (root == NULL)
         	return NULL;
-        if (root == p)
-			return p;
-		if (root == q)
-			return q;
+        if (root == p || root == q)
+			return root;
         
         TreeNode *left = lowestCommonAncestor(root->left, p, q); 
         TreeNode *right = lowestCommonAncestor(root->right, p, q);
@@ -22,4 +20,22 @@ public:
     }
 };
 
-
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode *ans = root;
+        int q_val = q->val;
+  		int p_val = p->val;
+		        
+        while (true) {
+        	if (ans->val > q_val && ans->val > p_val)
+        		ans = ans->left;
+			else if (ans->val < q_val && ans->val < p_val)
+				ans = ans->right;
+			else 
+				break;
+		}
+		
+		return ans;
+    }
+};
