18f43df2329609a1e6c87d5016a20607c13f851e
Dai-Wenxun <wxDai2001@gmail.com>
2021-12-23 22:54:01 +0800

    Create 785. 判断二分图.cpp

---
+++ 图/中等/785. 判断二分图.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+	vector<int> vtd;
+	vector<int> color;
+ 	bool res = true;
+    bool isBipartite(vector<vector<int>>& graph) {
+    	vtd.resize(graph.size(), 0);
+    	color.resize(graph.size(), 0);
+		for (int i = 0; i < graph.size(); ++i) {
+			if (!vtd[i]) 
+				traverse(graph, i);
+		}
+		return res;
+    }
+    
+    void traverse(vector<vector<int>>& graph, int cur) {
+    	if (!res) return;
+    	
+    	vtd[cur] = 1;
+    	
+    	for (int v: graph[cur]) {
+    		if (!vtd[v]) {
+    			color[v] = !color[cur];
+    			traverse(graph, v);
+			} else {
+				if (color[cur] == color[v]) {
+					res = false;
+					return ;
+				}	
+			} 
+		}
+	} 
+};
