0992203a662a6f16f25ce65292f83062983310d2
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-04 19:51:40 +0800

    Update 895. 最大频率栈.cpp

--- 数据结构设计/895. 最大频率栈.cpp
+++ 数据结构设计/895. 最大频率栈.cpp
@@ -3,37 +3,27 @@ public:
 	unordered_map<int, stack<int>> fv;
 	unordered_map<int, int> vf;  
 	int maxfreq = 0;
-    FreqStack() {
-		fv.clear();
-		vf.clear();
-	}
+    FreqStack() {}
     
     void push(int val) {
-		auto it = vf.find(val);
-		if (it == vf.end()) {
-			vf[val] = 1;
-			fv[1].push(val);
-			if (maxfreq == 0)
-				maxfreq = 1;
-		} else {
-			vf[val] += 1;
-			fv[vf[val]].push(val);
-			if (maxfreq < vf[val])
-				maxfreq = vf[val];
-		}
+		int& freq = vf[val];
+		freq++;
+		fv[freq].push(val);
+		maxfreq = max(freq, maxfreq); 
     }
     
     int pop() {
-		int res = fv[maxfreq].top();
-		fv[maxfreq].pop();
-		vf[res]--;
-		if (fv[maxfreq].empty()) {
+    	auto& sk = fv[maxfreq];
+		int res = sk.top();
+		sk.pop();
+		if (sk.empty()) {
 			fv.erase(maxfreq);
-			maxfreq--;
+			--maxfreq;
 		}
-		if (vf[res] == 0) {
+		int& freq = vf[res];
+		freq--;
+		if (!freq) 
 			vf.erase(res);
-		}
 		return res;
     }
 };
