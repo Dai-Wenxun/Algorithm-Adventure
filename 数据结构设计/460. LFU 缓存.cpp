d4e5a9b7a27fc9b0df6a8740f6ed64ebefb9765e
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-05 11:33:05 +0800

    引用不能重新赋值！！！！！！！！！

--- 数据结构设计/460. LFU 缓存.cpp
+++ 数据结构设计/460. LFU 缓存.cpp
@@ -1,7 +1,6 @@
 class Node {
 public:
 	int key, val, freq;
-	Node *next, *prev;
 	Node (int k, int v, int f): key(k), val(v), freq(f) {} 
 };
 
@@ -21,16 +20,18 @@ public:
     	if (capacity == 0) return -1;
 		auto it = key_table.find(key);
 		if (it == key_table.end()) return -1;
-		auto node = it->second;
+		auto& node = it->second;
 		int val = node->val; 
 		int freq = node->freq;
-		freq_table[freq].erase(node);
-		if (freq_table[freq].size() == 0) {
+		auto& nl = freq_table[freq];
+		nl.erase(node);
+		if (nl.size() == 0) {
 			freq_table.erase(freq);
 			if (minfreq == freq) minfreq += 1;
 		} 
-		freq_table[freq+1].emplace_front(key, val, freq+1);
-		key_table[key] = freq_table[freq+1].begin();
+		nl = freq_table[freq+1];
+		nl.emplace_front(key, val, freq+1);
+		key_table[key] = nl.begin();
 		return val;
     }
     
@@ -40,26 +41,30 @@ public:
     	
     	if (it == key_table.end()) {
     		if (key_table.size() == capacity) {
-    			auto node = freq_table[minfreq].back();
-    			key_table.erase(node.key);
-    			freq_table[minfreq].pop_back();
-    			if (freq_table[minfreq].size() == 0) {
+    			auto& nl = freq_table[minfreq];
+    			auto& n = nl.back();
+    			key_table.erase(n.key);
+    			nl.pop_back();
+    			if (nl.size() == 0) {
     				freq_table.erase(minfreq);
 				}
 			}
-			freq_table[1].emplace_front(key, value, 1);
-			key_table[key] = freq_table[1].begin();
+			auto& nl = freq_table[1];
+			nl.emplace_front(key, value, 1);
+			key_table[key] = nl.begin();
 			minfreq = 1;
 		} else {
-			list<Node>::iterator node = it->second;
+			auto& node = it->second;
 			int freq = node->freq;
-			freq_table[freq].erase(node);
-			if (freq_table[freq].size() == 0) {
+			auto& nl = freq_table[freq];
+			nl.erase(node);
+			if (nl.size() == 0) {
 				freq_table.erase(freq);
 				if (minfreq == freq) minfreq += 1;
 			} 
-			freq_table[freq+1].emplace_front(key, value, freq+1);
-			key_table[key] = freq_table[freq+1].begin();
+			nl = freq_table[freq+1];
+			nl.emplace_front(key, value, freq+1);
+			key_table[key] = nl.begin();
 		}
     }
 };
