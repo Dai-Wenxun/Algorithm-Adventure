75cc0d5c78d82b90589e74339790112161bf4ef8
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-13 21:58:46 +0800

    Update 300. 最长递增子序列.cpp

--- 动态规划/300. 最长递增子序列.cpp
+++ 动态规划/300. 最长递增子序列.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-		int max = 1;
 		int sz = nums.size();
 		vector<int> dp(sz, 1);
 		
@@ -16,5 +15,30 @@ public:
 		}
 		
 		return *max_element(dp.begin(), dp.end());
+    }
+};
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+		int sz = nums.size();
+		vector<int> tails(sz, 0);
+		int res = 0;
+		for (auto num: nums) {
+			int i = 0;
+			int j = res;
+			while (i < j) {
+				int m = (i + j) / 2;
+				if (tails[m] < num)
+					i = m + 1;
+				else
+					j = m;
+			}
+			
+			tails[i] = num;
+			if (j == res)
+				res += 1;
+		}
+		return res;
     }
 };
