e71c7a0c74cf3ea239a850b890eda318dd05e1f3
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-13 21:26:30 +0800

    Update 322. 零钱兑换.cpp

--- 动态规划/322. 零钱兑换.cpp
+++ 动态规划/322. 零钱兑换.cpp
@@ -1,27 +1,39 @@
 class Solution {
-public: 
-	unordered_map<int, int> mp;
+public:
+	vector<int> dp;
     int coinChange(vector<int>& coins, int amount) {
-    	return dp(coins, amount);  
+		dp.resize(amount+1, amount + 1);
+		int sz = dp.size(); 
+		
+		dp[0] = 0;
+		for (int i = 1; i < sz; ++i) {
+			for (auto coin: coins) {
+				if (i - coin < 0)
+					continue;
+				dp[i] = min(dp[i-coin] + 1, dp[i]);	
+			}
+		}
+		return dp[amount] == amount+1 ? -1 : dp[amount];
     }
     
-    int dp(vector<int>& coins, int amount) {
-    	if (amount == 0) return 0;
-    	if (amount < 0) return -1;
-    	
-    	auto it = mp.find(amount);
-		if (it != mp.end()) 
-			return it->second;
-
-		int res = INT_MAX;
-    	for (auto m: coins) {
-			int subProblem = dp(coins, amount-m);
-			if (subProblem == -1) continue;
-
-    		res = min(res, subProblem+1);
-		}
-		
-		mp[amount] = res == INT_MAX ? -1 : res;
-		return mp[amount];
-	}
+//    unordered_map<int, int> mp;
+//    int dp(vector<int>& coins, int amount) {
+//    	if (amount < 0) return -1;
+//    	if (amount == 0) return 0;
+//    	
+//    	if (mp[amount] != -2)
+//    		return mp[amount];
+//    	
+//    	int res = INT_MAX;
+//    	for (auto coin: coins) {
+//    		int subp = dp(coins, amount-coin);
+//    		if (subp == -1) continue;
+//    		
+//    		res = min(res, subp+1);
+//		}
+//		
+//		mp[amount] = res == INT_MAX ? -1 : res;
+//		
+//		return mp[amount];
+//	}
 };
