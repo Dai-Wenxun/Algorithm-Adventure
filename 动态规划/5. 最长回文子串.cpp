0d4f11f25c6f94ee2e4286de4511d92aef64e8a4
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-14 14:04:48 +0800

    Update 5. 最长回文子串.cpp

--- 动态规划/5. 最长回文子串.cpp
+++ 动态规划/5. 最长回文子串.cpp
@@ -8,7 +8,7 @@ public:
 			auto s2 = Palindrome(s, i, i+1);
 			
 			res = res.size() > s1.size() ? res : s1;
-			res = res.size() > s1.size() ? res : s1; 
+			res = res.size() > s2.size() ? res : s2; 
 		}
 		return res;
     }
@@ -21,3 +21,37 @@ public:
 		return s.substr(l+1, r-l-1);
 	}
 };
+
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+    	int sz = s.size();
+    	
+    	vector<vector<int>> dp(sz, vector<int>(sz, 0));
+    	
+    	int begin = 0, maxlen = 1;
+    	for (int i = 0; i < sz; ++i)
+    		dp[i][i] = 1;
+    	
+		for (int l = 2; l <= sz; ++l) {
+			for (int i = 0 ; i <= sz-l; ++i) {	
+				int j = l + i - 1;
+				
+				if (s[i] == s[j]) {
+					if (dp[i+1][j-1] || j - i < 3)	
+						dp[i][j] = 1;
+						
+					if (dp[i][j] && j - i + 1> maxlen) {
+						begin = i;
+						maxlen = j - i + 1;
+					}
+				} else
+					dp[i][j] = 0;
+			}
+		}
+		
+		return s.substr(begin, maxlen);
+    }
+};
+
