fae78be7240b8dffa43935aa51788e04c8a74a3c
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-15 12:18:19 +0800

    Update 931. 下降路径最小和.cpp

--- 动态规划/931. 下降路径最小和.cpp
+++ 动态规划/931. 下降路径最小和.cpp
@@ -8,21 +8,57 @@ public:
 			dp[0][i] = matrix[0][i];
 		
 		for (int i = 1; i < n; ++i) {
-			for (int j = 0; j < n; ++j) {
-				int min_sum = INT_MAX;
-				
-				int min_sum = matrix[i-1][j];
+			for (int j = 0; j < n; ++j) {			
+				int min_sum = dp[i-1][j];
 				if (j > 0)
 					min_sum = min(min_sum, dp[i-1][j-1]);
 				if (j < n-1)
 					min_sum = min(min_sum, dp[i-1][j+1]);
 				
-				dp[i][j] = min_sum;
+				dp[i][j] = min_sum + matrix[i][j];
+			}
+		}
+
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; ++j) {
+//                cout << dp[i][j] << " ";
+//            }
+//            cout << endl;
+//        }
+
+		int sum = INT_MAX;
+		for (int i = 0; i < n; ++i) 
+			sum = min(dp[n-1][i], sum); 
+		
+		return sum;
+    }
+};
+
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+		int n = matrix.size();
+		vector<int> dp(matrix[0].begin(), matrix[0].end());
+		
+		for (int i = 1; i < n; ++i) {
+			int pre = 0;
+			for (int j = 0; j < n; ++j) {
+				int tmp = dp[j];
+				
+				int min_sum = dp[j];
+				if (j > 0)
+					min_sum = min(min_sum, pre);
+				if (j < n-1)
+					min_sum = min(min_sum, dp[j+1]);
+				
+				dp[j] = min_sum + matrix[i][j];
+				
+				pre = tmp;
 			}
 		}
 		int sum = INT_MAX;
 		for (int i = 0; i < n; ++i) 
-			sum = min(dp[n-1][0], sum); 
+			sum = min(dp[i], sum); 
 		
 		return sum;
     }
