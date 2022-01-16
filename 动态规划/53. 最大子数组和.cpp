976a539dce46fe333f60026bf6dbf80f7ff32af3
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-16 13:20:16 +0800

    Update 53. 最大子数组和.cpp

--- 动态规划/53. 最大子数组和.cpp
+++ 动态规划/53. 最大子数组和.cpp
@@ -11,3 +11,21 @@ public:
     	return *max_element(dp.begin(), dp.end());
 	}
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+		int sz = nums.size();
+		int res = nums[0];
+		int pre = nums[0];
+		for (int i = 1; i < sz; ++i) {
+			if (pre + nums[i] > nums[i]) {
+				pre = pre + nums[i];	
+			} else {
+				pre = nums[i];
+			}
+			res = max(pre, res);
+		}	
+    	return res;
+	}
+};
