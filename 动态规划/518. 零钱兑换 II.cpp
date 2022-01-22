73487c2d050a3c6bd8aa80c7c919e51b0a3a0311
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-22 11:23:12 +0800

    Create 518. 零钱兑换 II.cpp

---
+++ 动态规划/518. 零钱兑换 II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+		int sz = coins.size();
+		
+		vector<vector<int>> dp(sz+1, vector<int>(amount+1, 0));
+		
+		for (int i = 0; i <= sz; ++i)
+			dp[i][0] = 1;
+				
+		for (int i = 1; i <= sz; ++i) {
+			for (int j = 1; j <= amount; ++j) {
+				if (j - coins[i-1] < 0)
+					dp[i][j] = dp[i-1][j];
+				else
+					dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]];
+			}
+		}
+		return dp[sz][amount];
+    }
+};
