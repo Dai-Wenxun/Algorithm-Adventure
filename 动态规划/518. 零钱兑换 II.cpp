996951024fea6ea9743309bf5c076ce7e127f5d4
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-22 11:28:43 +0800

    Update 518. 零钱兑换 II.cpp

--- 动态规划/518. 零钱兑换 II.cpp
+++ 动态规划/518. 零钱兑换 II.cpp
@@ -19,3 +19,25 @@ public:
 		return dp[sz][amount];
     }
 };
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+		int sz = coins.size();
+		
+		vector<int> dp(amount+1, 0);
+		
+		dp[0] = 1;		
+		for (int i = 1; i <= sz; ++i) {
+			for (int j = 1; j <= amount; ++j) {
+				if (j - coins[i-1] >= 0)
+					dp[j] = dp[j] + dp[j-coins[i-1]];
+			}
+		}
+		return dp[amount];
+    }
+};
+
+
+
+
