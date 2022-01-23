73a8dbbe6a22ad2020615063fd0ab38671d11c8d
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-23 10:09:09 +0800

    Update 64. 最小路径和.cpp

--- 动态规划/64. 最小路径和.cpp
+++ 动态规划/64. 最小路径和.cpp
@@ -44,3 +44,36 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+		int m = grid.size();
+		int n = grid[0].size(); 
+		
+		vector<vector<int>> dp(m, vector<int>(n));
+		
+		int sum = 0;
+		
+		for (int j = 0; j < n; ++j) {
+			sum += grid[0][j];
+			dp[0][j] = sum;
+		}
+		
+		sum = 0;
+		for (int i = 0; i < m; ++i) {
+			sum += grid[i][0]; 
+			dp[i][0] = sum;
+		}
+		
+		for (int i = 1; i < m; ++i) {
+			for (int j = 1; j < n; ++j) {
+				dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+			}
+		}
+		
+		return dp[m-1][n-1];
+    }
+};
+
+
