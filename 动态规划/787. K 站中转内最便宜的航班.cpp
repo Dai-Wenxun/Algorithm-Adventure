e2d22fcc7b7f603dd5042f2151289a9ec87abf1f
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-29 17:26:36 +0800

    Update 787. K 站中转内最便宜的航班.cpp

--- 动态规划/787. K 站中转内最便宜的航班.cpp
+++ 动态规划/787. K 站中转内最便宜的航班.cpp
@@ -38,15 +38,12 @@ public:
     int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
 		vector<vector<pii>> g(n);
 		memo = vector<vector<int>>(n, vector<int>(k+2));
-		for (auto& x: f) 
-			g[x[0]].emplace_back(x[1], x[2]);
-	
 		dfs(g, k+1, src, dst);
 		
 		return memo[src][k+1] >= INF ? -1 : memo[src][k+1];
     }
     
-    int dfs(vector<vector<pii>>& g, int k, int cur, int dst) {
+    int dfs(vector<vector<int>>& f, int k, int cur, int dst) {
 		if (k < 0)
 			return INF;
     	
@@ -57,12 +54,33 @@ public:
 			return 0;
 		
 		int res = INF;
-    	for (auto& p: g[cur]) {
-    		res = min(dfs(g, k-1, p.first, dst)+p.second, res);
+    	for (auto& p: f) {
+    		if (p[0] == cur) 
+    			res = min(dfs(f, k-1, p[1], dst)+p[2], res);
 		}
 		
 		memo[cur][k] = res; 
 		return res;
 	}
+};
+
+
+
+class Solution {
+public:
+	
+	static constexpr int INF = 1000007;
+	
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+		vector<vector<int>> dp(n, vector<int>(k+2, INF));
+		dp[dst][0] = 0;
+		for (int i = 1; i <= k+1; ++i) {
+			for (auto& f: flights) {
+				dp[f[0]][i] = min(dp[f[1]][i-1] + f[2], dp[f[0]][i]);
+			}	
+		}
+		int res = *min_element(dp[src].begin(), dp[src].end());
+		return res == INF ? -1 : res;
+    }
 };
 
