95d3eeefac334815dbec185aa542784e6b04a54e
Dai-Wenxun <wxDai2001@gmail.com>
2022-01-29 16:15:00 +0800

    Update 787. K 站中转内最便宜的航班.cpp

--- 动态规划/787. K 站中转内最便宜的航班.cpp
+++ 动态规划/787. K 站中转内最便宜的航班.cpp
@@ -27,3 +27,42 @@ public:
 		return ans[dst] == INT_MAX ? -1 : ans[dst];
     }
 };
+
+typedef pair<int, int> pii;
+
+class Solution {
+public:
+	static constexpr int INF = 1000007;
+	
+	vector<vector<int>> memo;
+    int findCheapestPrice(int n, vector<vector<int>>& f, int src, int dst, int k) {
+		vector<vector<pii>> g(n);
+		memo = vector<vector<int>>(n, vector<int>(k+2));
+		for (auto& x: f) 
+			g[x[0]].emplace_back(x[1], x[2]);
+	
+		dfs(g, k+1, src, dst);
+		
+		return memo[src][k+1] >= INF ? -1 : memo[src][k+1];
+    }
+    
+    int dfs(vector<vector<pii>>& g, int k, int cur, int dst) {
+		if (k < 0)
+			return INF;
+    	
+    	if (memo[cur][k])
+    		return memo[cur][k];
+    	
+    	if (cur == dst)
+			return 0;
+		
+		int res = INF;
+    	for (auto& p: g[cur]) {
+    		res = min(dfs(g, k-1, p.first, dst)+p.second, res);
+		}
+		
+		memo[cur][k] = res; 
+		return res;
+	}
+};
+
