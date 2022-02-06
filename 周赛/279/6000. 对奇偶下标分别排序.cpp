38bd8f21a56fa1f6cdf22b88c22e2441cc6e54b9
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-06 10:53:00 +0800

    Create 6000. 对奇偶下标分别排序.cpp

---
+++ 周赛/279/6000. 对奇偶下标分别排序.cpp
@@ -0,0 +1,33 @@
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+typedef pair<int, int> PII;
+typedef long long LL;
+
+class Solution {
+public:
+    vector<int> sortEvenOdd(vector<int>& nums) {
+        vector<int> evens, odds, res;
+		
+		int sz = nums.size();
+		for (int i = 0; i < sz; ++i)
+		 	if (i % 2 == 0) evens.pb(nums[i]);
+    		else odds.pb(nums[i]);
+    	
+    	sort(evens.begin(), evens.end());
+    	sort(odds.begin(), odds.end(), greater<int>());
+    	
+//    	for (int i = 0; i < evens.size(); ++i)
+//    		cout << evens[i] << " ";
+//    	
+//        for (int i = 0; i < odds.size(); ++i)
+//    		cout << odds[i] << " ";
+    
+    	for (int i = 0; i < sz; ++i)
+    		if (i % 2 == 0) res.pb(evens[i/2]);
+    		else res.pb(odds[i/2]);
+    	
+    	return res;
+	}		
+};
