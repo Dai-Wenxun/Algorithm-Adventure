fba2e7a12c2819e639d9f16f8809ea46651c3b44
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-06 11:28:39 +0800

    Create 6001. 重排数字的最小值.cpp

---
+++ 周赛/279/6001. 重排数字的最小值.cpp
@@ -0,0 +1,48 @@
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+typedef pair<int, int> pii;
+typedef long long ll;
+
+class Solution {
+public:
+    long long smallestNumber(long long num) {
+        if (num == 0) return 0;
+        ll res = 0;
+		vector<int> nums;
+        int flag = num < 0 ? -1 : 1;
+        while (num != 0) {
+        	nums.pb(num%10);
+        	num /= 10;
+		}
+        sort(nums.begin(), nums.end());
+        int sz = nums.size();
+        if (flag == -1) {
+        	for (int i = 0; i < sz; ++i) {
+        		int cur = nums[i];
+        		cout << cur << " " << endl;
+        		if (cur == 0) continue;
+        		res += nums[i] * (ll)pow(10, sz-1-i);
+        		cout << res << " ";
+			}
+			cout << res;
+		} else {
+			int see = 0;
+			for (int i = 0; i < sz; ++i) {
+				int cur = nums[i];
+				if (cur == 0) continue;
+				if (see == 0) {
+						res += nums[i] * (ll)pow(10, sz-1);
+						see = 1;
+					} else {
+						res += nums[i] * (ll)pow(10, sz-1-i);
+					}
+			}
+		}        
+//        for (int i = 0; i < nums.size(); ++i)
+//        	cout << nums[i] << " ";
+
+    	return res;
+    }
+};
