c534bc5ae27a60e80304b24829959bae576c488c
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-06 14:54:06 +0800

    Create 6002. 设计位集.cpp

---
+++ 周赛/279/6002. 设计位集.cpp
@@ -0,0 +1,47 @@
+#define pb push_back
+#define fi first
+#define se second
+#define mp make_pair
+typedef pair<int, int> pii;
+typedef long long ll;
+
+class Bitset {
+public:
+    #define N 100005
+	bool a[N];
+	int b[2],nowsiz,now;
+    Bitset(int size) {
+		nowsiz=size;now=0;
+		for (int i=1;i<=size;i++) a[i]=0;
+		b[0]=size;
+		b[1]=0;
+    }
+    
+    void fix(int idx) {
+    	idx++;
+    	b[a[idx]^now]--;
+		a[idx]=now^1;
+		b[a[idx]^now]++;
+    }
+    
+    void unfix(int idx) {
+    	idx++;
+    	b[a[idx]^now]--;
+		a[idx]=now;
+		b[a[idx]^now]++;
+    }
+    
+    void flip() {now^=1,swap(b[0],b[1]);}
+    
+    bool all() {return b[1]==nowsiz;}
+    
+    bool one() {return !!b[1];}
+    
+    int count() {return b[1];}
+    
+    string toString() {
+		string s;
+		for (int i = 1; i <= nowsiz; i++)if((a[i]^now)==1)s+='1'; else s+='0';
+		return s;
+    }
+};
