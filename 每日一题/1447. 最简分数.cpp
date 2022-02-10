690b5dff819d4f4daed1beef22c8e00c2fd474d6
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-10 23:48:35 +0800

    Update 1447. 最简分数.cpp

--- 每日一题/1447. 最简分数.cpp
+++ 每日一题/1447. 最简分数.cpp
@@ -14,3 +14,19 @@ public:
 		return res;
     }
 };
+
+class Solution {
+public:	
+	int gcd(int a, int b) { // 欧几里得算法
+	    return b == 0 ? a : gcd(b, a % b);
+	}
+    vector<string> simplifiedFractions(int n) {
+		vector<string> res;
+		for (int i = 2; i <= n; ++i) {
+			for (int j = 1; j <= i-1; ++j) {
+				if (gcd(i, j) == 1) res.pb(to_string(i) + '/' + to_string(i));
+			}	
+		}
+		return res;
+    }
+};
