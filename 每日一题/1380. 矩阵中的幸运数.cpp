07a65b4fa34777f176773482766fc018af743ac1
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-15 11:54:21 +0800

    Update 1380. 矩阵中的幸运数.cpp

--- 每日一题/1380. 矩阵中的幸运数.cpp
+++ 每日一题/1380. 矩阵中的幸运数.cpp
@@ -24,3 +24,22 @@ public:
 		return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> luckyNumbers (vector<vector<int>>& matrix) {
+		int m = SZ(matrix), n = SZ(matrix[0]); 
+		vector<int> rows(m, 2e9), cols(n, -2e9), res;	
+		rep(i, 0, m-1) {
+			rep(j, 0, n-1) {
+				rows[i] = min(rows[i], matrix[i][j]);
+				cols[j] = max(cols[j], matrix[i][j]);
+			}
+		}	
+		rep(i, 0, m-1)
+			rep(j, 0, n-1) 
+				if (matrix[i][j] == rows[i] && matrix[i][j] == cols[j])
+					res.pb(matrix[i][j]);
+		return res;
+    }
+};
