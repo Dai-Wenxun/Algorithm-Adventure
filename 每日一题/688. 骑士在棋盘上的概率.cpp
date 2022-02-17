16e91d4ad14ea057ac56d9f58624e4fbbfa1790b
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-17 12:10:15 +0800

    Update 688. 骑士在棋盘上的概率.cpp

--- 每日一题/688. 骑士在棋盘上的概率.cpp
+++ 每日一题/688. 骑士在棋盘上的概率.cpp
@@ -22,8 +22,8 @@ public:
     		if (x >= sz || x < 0 || y >= sz || y < 0)
     			continue;
     		
-    		if (memo[x][y][k] != -6666)
-    			s += memo[x][y][k];
+    		if (memo[x][y][k-1] != -6666)
+    			s += memo[x][y][k-1] / 8;
     		else
     			s += 0.125 * dfs(x, y, k-1);
     			
@@ -33,4 +33,24 @@ public:
 		memo[r][c][k] = s;
 		return s;
 	}
+};
+
+class Solution {
+public:
+	int d[8][2] = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}}; 
+	double knightProbability(int n, int k, int row, int column) {
+		vector<vector<vector<double>>> dp(n, vector<vector<double>>(n, vector<double>(k+1))};
+		rep(i, 0, n-1) rep(j, 0, n-1) dp[i][j][0] = 1;
+		
+		rep(p, 1, k) rep(i, 0, n-1) rep(j, 0, n-1) rep(index, 0, 7) {
+			int x = i + d[index][0];
+    		int y = j + d[index][1];
+    		if (x >= sz || x < 0 || y >= sz || y < 0)
+    			continue;
+    		
+    		dp[i][j][p] += dp[x][y][p-1] / 8;
+		}
+		
+		return dp[row][column][k];
+    }
 };
