250d52bb9208d47007611b2cf269ab1150f8204b
Dai-Wenxun <wxDai2001@gmail.com>
2022-02-18 19:31:37 +0800

    Update 121. 买卖股票的最佳时机.cpp

--- 动态规划/121. 买卖股票的最佳时机.cpp
+++ 动态规划/121. 买卖股票的最佳时机.cpp
@@ -12,3 +12,21 @@ public:
 		return *max_element(res.begin(), res.end());
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+    	int sz = SZ(prices);
+		vector<vector<int>> dp(sz, vector<int>(2));
+		
+		dp[0][0] = 0;
+		dp[0][1] = -prices[0];
+		
+		rep(i, 1, sz-1) {
+			dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]);
+			dp[i][1] = max(dp[i-1][1], -prices[i]);
+		}
+		
+		return dp[sz-1][0];
+    }
+};
